9cb1a7df942f16b4357426284030259cb88bff83
node104 <44588689+node104@users.noreply.github.com>
2018-11-06 03:10:21 +0530

    new commit

--- push.sh
+++ push.sh
@@ -2,6 +2,6 @@ git add .
 git commit -m "new commit"
 git remote rm origin
 git remote add origin git@github.com:node104/AttackGraph-2018.git
-git push -u origin master
+git push -f origin master
 
 
